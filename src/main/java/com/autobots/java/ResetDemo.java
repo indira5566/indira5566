baf3fc9c1e5b98da50f9da389bd5f66aadaa9689
Indira <tynalievaindira09@gmail.com>
2025-07-08 09:35:19 +0600

    version 1.0.0

---
+++ src/main/java/com/autobots/java/ResetDemo.java
@@ -0,0 +1,10 @@
+package com.autobots.java;
+
+public class ResetDemo {
+    public static void main(String[] args) {
+        System.out.println("Version 1.0.0");
+        System.out.println("Version 1.0.1");
+        System.out.println("Version 1.0.2");
+        System.out.println("Version 1.0.3");
+    }
+}
